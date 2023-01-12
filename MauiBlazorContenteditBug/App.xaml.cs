5603de51c3bb8ed7acc95db1f7645e58eabe10c5
dady8889 <dady8889@gmail.com>
2023-01-12 01:40:19 +0100

    Initial commit

---
+++ MauiBlazorContenteditBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorContenteditBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiBlazorContenteditBug/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorContenteditBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
